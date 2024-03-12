0bbbe865207524fafb3a004e9ebb2c7e00d6404c
sahil kushwaha <sahilkushwaha5510@gmail.com>
2024-03-12 14:53:51 +0530

    add multer middleware and cloudinary(could service) utility

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import multer from 'multer'
+
+// configure multer with diskStorage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+       cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+       cb(null, `${Date.now()}-${file.originalname}`)
+    }
+  })
+
+
+export const upload=multer({storage:storage});
+
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const cloudinaryUploadFile=async (localFilePath)=>{
+                try{
+                       if(!localFilePath) return null;
+                      //upload file on cloudinary
+                      const response=await cloudinary.uploader.upload(localFilePath,
+                      {
+                         resource_type:"auto" 
+                      })        
+                     // file has been uploaded successfully
+                      console.log(response.url)
+                      return response;
+                }   
+                
+               catch(error){
+                    //remove the locally saved temprory file as the upload operartion got failed
+                    fs.unlinkSync(localFilePath) 
+                    return null;
+               } 
+}
+
+export {cloudinaryUploadFile}
